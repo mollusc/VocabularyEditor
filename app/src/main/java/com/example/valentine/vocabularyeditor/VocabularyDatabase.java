82a1fd6af558284b0e0dbc9080ce4e66469be367
mollusc <MolluscLab@gmail.com>
2014-10-27 22:01:18 +0400

    Working on database reader

--- app/src/main/java/com/example/valentine/vocabularyeditor/VocabularyDatabase.java
+++ app/src/main/java/com/example/valentine/vocabularyeditor/VocabularyDatabase.java
@@ -1,10 +1,12 @@
 package com.example.valentine.vocabularyeditor;
 
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.preference.PreferenceManager;
 
 import java.io.File;
+import java.util.Dictionary;
 
 /**
  * Interface with vocabulary's database
@@ -17,8 +19,18 @@ public class VocabularyDatabase {
     public VocabularyDatabase(String pathToVocabulary) {
         pathToVocabulary = "/mnt/sdcard/Android/data/com.dropbox.android/files/scratch/LinguaSubtitle/Vocabulary.db";
         File dbfile = new File(pathToVocabulary);
-        db = SQLiteDatabase.openOrCreateDatabase(dbfile, null);
-       // db = SQLiteDatabase.openDatabase(pathToVocabulary, null, SQLiteDatabase.OPEN_READWRITE);
-        int i = 4+6;
+        //db = SQLiteDatabase.openOrCreateDatabase(dbfile, null);
+        db = SQLiteDatabase.openDatabase(pathToVocabulary, null, 0);
     }
+
+    // Функция по получения 15 кратных слов, в качестве возвращ значения
+    // использовать список классов Word как и в LinguaSubtitle
+    //public  Get15
+    // String selectQuery = "SELECT lastchapter FROM Bookdetails WHERE bookpath=?";
+    // Cursor c = db.rawQuery(selectQuery, new String[] { fileName });
+    // if (c.moveToFirst()) {
+    //    temp_address = c.getString(c.getColumnIndex("lastchapter"));
+    //}
+    //c.close();
+
 }
